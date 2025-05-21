e9edd11eee9215e2e936e650278f0fc9a940a7eb
Igland <igland@gmail.com>
2025-05-21 11:25:03 +0200

    asd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-theme="cupcake">
+    <html lang="en" data-theme="dark">
       <body
         className={`
           ${geistSans.variable} ${geistMono.variable}
