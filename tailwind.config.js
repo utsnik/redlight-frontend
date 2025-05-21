97920f7a545e724b698bcb0484861eaf6d92863d
Igland <igland@gmail.com>
2025-05-21 14:03:52 +0200

    asd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,24 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  plugins: [
-    require("daisyui"),
-  ],
+  plugins: [ require("daisyui") ],
   daisyui: {
     themes: [
       {
         redlight: {
-          primary:         "#f59e0b",
+          primary:          "#f59e0b",
           "primary-content":"#1f2937",
-          secondary:       "#10b981",
-          accent:          "#6366f1",
-          neutral:         "#111827",
-          "base-100":      "#f3f4f6",
-          info:            "#3b82f6",
-          success:         "#22c55e",
-          warning:         "#f97316",
-          error:           "#ef4444",
+          secondary:        "#10b981",
+          accent:           "#6366f1",
+          neutral:          "#111827",
+          "base-100":       "#f3f4f6",
+          info:             "#3b82f6",
+          success:          "#22c55e",
+          warning:          "#f97316",
+          error:            "#ef4444",
         },
       },
       "dark",
