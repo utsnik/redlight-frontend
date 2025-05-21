5fed9fc08da251117821c9732b0e64b46b52d048
Igland <igland@gmail.com>
2025-05-21 11:35:14 +0200

    asd

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},  // use the standalone PostCSS plugin
+    autoprefixer: {},            // Next.js needs this for vendor prefixes
   },
-};
+}
