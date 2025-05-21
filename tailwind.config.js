0f2ecaa7878095f7c798eef46be2a5cae25b28d0
Igland <igland@gmail.com>
2025-05-21 11:41:36 +0200

    asd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 // tailwind.config.js
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  plugins: [
-    require("daisyui"),
-  ],
+  plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light","dark","cupcake"],
+    themes: ["light", "dark", "cupcake"],
   },
 };
